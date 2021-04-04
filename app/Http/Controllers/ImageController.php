edc2321380a8a5ce2ed7d769f82f493470fa40e7
Aurelio Rojas <aureliogg911@gmail.com>
2021-04-04 18:47:29 -0400

    Default image fix

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,35 +8,28 @@ use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    public function retrieveImage($id)
-    {
-        $image = "images/default.jpg";
-        if ($id !== 'undefined') {
-            $image = Image::find($id)->path;
-        }
-
+    public function retrieveImage($id){
+        $image = Image::find($id)->path;
         return Storage::download($image);
     }
 
-    public function retrievePlaceholderImage($id)
-    {
+    public function retrievePlaceholderImage($id){
         $image = Image::find($id)->preview_path;
         return Storage::download($image);
     }
 
-    public function updateImage(Request $request, $id)
-    {
+    public function updateImage(Request $request, $id){
         $image_entry = Image::find($id);
-        if (isset($image_entry)) {
+        if(isset($image_entry)){
             $result = $image_entry->delete();
             return $result;
         }
     }
-    public function destroy($id)
-    {
+    public function destroy($id) {
         $image = Image::findOrFail($id);
         Storage::delete([$image->preview_path, $image->path]);
         $image->delete();
-        return $image;
+        return $image;        
     }
 }
+
